cf98a90a06b54d0d70a8165ecf108bc2bb7345ed
A.Kyzmenko <a.kyzmenko@MacBook-Air-Anna.local>
2020-03-31 18:22:37 +0300

    clear interceptors

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import React from 'react';
-import './App.css';
-
+import React from "react";
+import "./App.css";
 
 //Components
-import Layot from './HOC/Layot/Layot'
-import BurgerBuilder from './views/BurgerBuilder/BurgerBuilder'
-import Logout from './views/Auth/Logout/Logout'
+import Layot from "./HOC/Layot/Layot";
+import BurgerBuilder from "./views/BurgerBuilder/BurgerBuilder";
+import Logout from "./views/Auth/Logout/Logout";
 
-const Orders = React.lazy(()=>{
-  return import ('./views/Orders/Orders')
-})
-const Checkout = React.lazy(()=>{
-  return import('./views/Checkout/Checkout')
-})
-const Auth = React.lazy(()=>{
-  return import('./views/Auth/Auth')
-})
-
-
+const Orders = React.lazy(() => {
+  return import("./views/Orders/Orders");
+});
+const Checkout = React.lazy(() => {
+  return import("./views/Checkout/Checkout");
+});
+const Auth = React.lazy(() => {
+  return import("./views/Auth/Auth");
+});
 
 function App() {
   return (
-    <div >
-      <Layot><BurgerBuilder/></Layot>
+    <div>
+      <Layot>
+        <BurgerBuilder />
+      </Layot>
     </div>
   );
 }
--- src/HOC/WithErrorHandler.jsx
+++ src/HOC/WithErrorHandler.jsx
@@ -1,43 +1,50 @@
-import React, { Component } from 'react';
-import Modal from '../components/UI/Modal/Modal';
+import React, { Component } from "react";
+import Modal from "../components/UI/Modal/Modal";
 
-
-const withErrorHandler = ( WrappedComponent, axios ) => {
-    return class extends Component {
-        state = {
-            error: null
+const withErrorHandler = (WrappedComponent, axios) => {
+  return class extends Component {
+    state = {
+      error: null
+    };
+    constructor(props) {
+      super(props);
+      this.requestInterceptor = axios.interceptors.request.use(req => {
+        this.setState({ error: null });
+        return req;
+      });
+      this.responceInterceptor = axios.interceptors.response.use(
+        res => res,
+        error => {
+          this.setState({ error: error });
         }
-        constructor (props) {
-          super(props)
-            axios.interceptors.request.use(req => {
-                this.setState({error: null});
-                return req;
-            });
-            axios.interceptors.response.use(res => res, error => {
-                this.setState({error: error});
-            });
-        }
-
-        errorConfirmedHandler = () => {
-            this.setState({error: null});
-        }
-
-        render () {
-            return (
-                <React.Fragment>
-                    <Modal 
-                        show={this.state.error}
-                        modalClosed={this.errorConfirmedHandler}>
-                        {this.state.error ? this.state.error.message : null}
-                    </Modal>
-                    <WrappedComponent {...this.props} />
-                </React.Fragment>
-            );
-        }
+      );
     }
-}
 
+    errorConfirmedHandler = () => {
+      this.setState({ error: null });
+    };
 
+    componentWillUnmount() {
+      console.log("Will unm", this.requestInterceptor, this.responceInterceptor)
+      axios.interceptors.request.eject(this.requestInterceptor);
+      axios.interceptors.response.eject(this.responceInterceptor);
+    }
+
+    render() {
+      return (
+        <React.Fragment>
+          <Modal
+            show={this.state.error}
+            modalClosed={this.errorConfirmedHandler}
+          >
+            {this.state.error ? this.state.error.message : null}
+          </Modal>
+          <WrappedComponent {...this.props} />
+        </React.Fragment>
+      );
+    }
+  };
+};
 
 // const  WithErrorHandler = (WrappedComponent, axios) => {
 //   return (props)=>(
