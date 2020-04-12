d992e0b7ee969b61abec736afdc2c66faf94cd4e
A.Kyzmenko <a.kyzmenko@192.168.0.107.local.ipnet.ua>
2020-04-12 08:32:40 +0300

    added App hook

--- src/App.js
+++ src/App.js
@@ -1,68 +1,60 @@
-import React from "react";
-import "./App.css";
-import {Route, Switch,Redirect} from 'react-router-dom'
-import Orders from './views/Orders/Orders'
-import {connect} from 'react-redux'
-import {authCheckState} from './store/actions/authAction'
-
-
+import React, {useEffect} from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import { authCheckState } from "./store/actions/authAction";
 //Components
 import Layot from "./HOC/Layot/Layot";
 import BurgerBuilder from "./views/BurgerBuilder/BurgerBuilder";
-import Checkout from './views/Checkout/Checkout'
-import Logout from "./views/Auth/Logout/Logout"
-import Auth from './views/Auth/Auth';
 
-// const Orders = React.lazy(() => {
-//   return import("./views/Orders/Orders");
-// });
-// const Checkout = React.lazy(() => {
-//   return import("./views/Checkout/Checkout");
-// });
-// const Auth = React.lazy(() => {
-//   return import("./views/Auth/Auth");
-// });
+const Orders = React.lazy(() => {
+  return import("./views/Orders/Orders");
+});
+const Checkout = React.lazy(() => {
+  return import("./views/Checkout/Checkout");
+});
+const Auth = React.lazy(() => {
+  return import("./views/Auth/Auth");
+});
+const Logout = React.lazy(() => {
+  return import("./views/Auth/Logout/Logout");
+});
 
-class App extends React.Component {
-  componentDidMount() {
-    this.props.authCheckState();
-    console.log("app")
 
-  }
+const App = (props) =>{
   
+  useEffect(() => {
+    props.authCheckState();
+  }, [])
 
-  render() {
-    let routes = (
-      <Switch><Route exact path="/" component={BurgerBuilder} />
-    <Route path="/auth" component={Auth}/>
-
-    <Redirect to="/"/>
+  let routes = (
+    <Switch>
+      <Route exact path="/" component={BurgerBuilder} />
+      <Route path="/auth" component={Auth} />
+      <Redirect to="/" />
     </Switch>
-    )
+  );
 
-    if(this.props.token){
-      routes =(   <Switch>
+  if (props.token) {
+    routes = (
+      <Switch>
         <Route exact path="/" component={BurgerBuilder} />
         <Route path="/orders" component={Orders} />
-        <Route path="/checkout" component={Checkout}/>
-        <Route path="/logout" component={Logout}/>
-        <Route path="/auth" component={Auth}/>
-        <Redirect to="/"/>
-        </Switch>)
-    }
-    return (
-      <div>
-        <Layot>
-         {routes}
-        </Layot>
-      </div>
+        <Route path="/checkout" component={Checkout} />
+        <Route path="/logout" component={Logout} />
+        <Route path="/auth" component={Auth} />
+        <Redirect to="/" />
+      </Switch>
     );
   }
+  return (
+    <Layot>{routes}</Layot>
+  )
 }
+
 const mapStateToProps = (state) => ({
-  token:state.auth.token !== null
-})
+  token: state.auth.token !== null,
+});
+
+export default connect(mapStateToProps, { authCheckState })(App);
 
 
-
-export default connect(mapStateToProps,{authCheckState})(App);
